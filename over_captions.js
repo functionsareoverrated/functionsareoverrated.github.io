6370887fbc8a27399814629daf95783b27dca072
functionsareoverrated <49105098+functionsareoverrated@users.noreply.github.com>
2019-03-30 21:23:13 -0400

    Add files via upload

---
+++ over_captions.js
@@ -0,0 +1,15 @@
+var over_captions = {
+  before: [
+    "#TeamTuringMachine",
+    "something something VALUE RESTRICTION something something",
+    "who even cares at this point",
+    "NOT IN MY LOGIC",
+    "the hats drove me to this",
+    "CPS more like",
+    "functions are mappings from sets to sets",
+    "SETS EDUCATION is more important",
+    "something something REGULAR EXPRESSIONS something something",
+    "IM TALKIN BOUT SETS BABY, IM TALKIN BOUT YOU AND ME"
+   
+  ]
+}
