9e4b9eb9f0fdf5183e129568b97f04b950a0305f
functionsareoverrated <49105098+functionsareoverrated@users.noreply.github.com>
2019-03-31 20:03:52 -0400

    Update over_captions.js

--- over_captions.js
+++ over_captions.js
@@ -5,11 +5,12 @@ var over_captions = {
     "who even cares at this point",
     "NOT IN MY LOGIC",
     "the hats drove me to this",
-    "CPS more like",
+    "when would we ever need CPS",
     "functions are mappings from sets to sets",
     "SETS EDUCATION is more important",
     "something something REGULAR EXPRESSIONS something something",
-    "IM TALKIN BOUT SETS BABY, IM TALKIN BOUT YOU AND ME"
-   
+    "IM TALKIN BOUT SETS BABY, IM TALKIN BOUT YOU AND ME",
+    "According to all known laws of aviation, there is no way a bee should be able to fly. It's wings are too small to get its fat little body off the ground. The bee, of course, flies anyway, because bees don't care what humans think is impossible."
+    
   ]
 }
