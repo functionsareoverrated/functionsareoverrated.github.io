0c75527a23e9849cb993f80fffa788cf40ccc473
functionsareoverrated <49105098+functionsareoverrated@users.noreply.github.com>
2019-04-01 02:31:17 -0400

    Update over_captions.js

--- over_captions.js
+++ over_captions.js
@@ -3,14 +3,24 @@ var over_captions = {
     "#TeamTuringMachine",
     "something something VALUE RESTRICTION something something",
     "who even cares at this point",
-    "NOT IN MY LOGIC",
+    ' "NOT IN MY LOGIC" - Klaus',
     "the hats drove me to this",
     "when would we ever need CPS",
     "functions are mappings from sets to sets",
     "SETS EDUCATION is more important",
     "something something REGULAR EXPRESSIONS something something",
     "IM TALKIN BOUT SETS BABY, IM TALKIN BOUT YOU AND ME",
-    "According to all known laws of aviation, there is no way a bee should be able to fly. It's wings are too small to get its fat little body off the ground. The bee, of course, flies anyway, because bees don't care what humans think is impossible."
+    "According to all known laws of aviation, there is no way a bee should be able to fly. It's wings are too small to get its fat little body off the ground. The bee, of course, flies anyway, because bees don't care what humans think is impossible.",
+    "lambda functions? just cheap eigenvalues",
+    "analysis >>> program analysis",
+    "calculus >>> sequent calculus",
+    "just kidding about the calculus thing; calculus sucks and sequent calculus seems cooloer i'll give you that much",
+    "functional analysis >>> functions",
+    "polynomials >>> polymorphisms",
+    "C0 and SML aren't even real languages",
+    "#TeamFiniteStateMachines",
+    "#Team No one because this entire thing is ridiculous",
+    
     
   ]
 }
